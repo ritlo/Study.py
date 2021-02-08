2608fc09300c8683ad1020154dcac805d16a2780
Ritvick Lohani <lohaniritvick@gmail.com>
2021-02-08 23:34:50 +0530

    initial git release

---
+++ study.py
@@ -0,0 +1,60 @@
+import keyboard
+import time
+import pyttsx3
+import datetime
+import threading
+def scroll():
+    global sd
+    while True:
+        try:
+            should = input("Scroll? Y/N \n")
+            should = should.lower()
+            if should == "n":
+                sd = False
+                break
+            elif should == "y":
+                sd = True
+                break
+            else:
+                raise Exception("Try again")
+        except:
+            continue        
+scroll()
+i = int(input("Interval in seconds\n"))
+t = datetime.datetime.now()
+td = datetime.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+
+def speak(hrs, mins, secs):
+    engine = pyttsx3.init()
+    engine.say("Time")
+    if hrs > 0:
+        if hrs > 1:
+            engine.say(f"{hrs} Hours")
+        else:
+            engine.say(f"{hrs} Hour")
+    if mins > 0:
+        if mins > 1:
+            engine.say(f"{mins} Minutes")
+        else:
+            engine.say(f"{mins} Minute")
+    if secs > 0:
+        if secs > 1:
+            engine.say(f"{secs} Seconds")
+        else:
+            engine.say(f"{secs} Second")
+    engine.runAndWait()
+
+while True:
+
+    ti = datetime.datetime.now()
+    tid = datetime.timedelta(hours=ti.hour, minutes=ti.minute, seconds=ti.second)
+    tx = tid - td
+    secs = tx.seconds % 60
+    hrs = tx.seconds // 3600
+    mins = (tx.seconds // 60) % 60
+    # print(f"{hrs} {mins} {secs}")
+    th = threading.Thread(target=speak, args=(hrs, mins, secs))
+    if sd:
+        keyboard.press_and_release("page down")
+    th.start()
+    time.sleep(i)
